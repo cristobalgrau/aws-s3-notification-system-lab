4fd5f1d6924a33ce48f9b34825aeafb13672eb67
Cristobal Grau <cristobalgrau@gmail.com>
2024-02-24 19:24:30 +0000

    #7 Created lambda - lamda iam role and python code for lambda

--- main.tf
+++ main.tf
@@ -51,3 +51,57 @@ resource "aws_sqs_queue" "project_sqs" {
   message_retention_seconds = 86400
   receive_wait_time_seconds = 0
 }
+
+# ==== LAMBDA SECTION ====
+
+# Creation of Lambda Function
+resource "aws_lambda_function" "project_lambda" {
+  filename      = "${path.module}/lambda/lambda_function_payload.zip"
+  function_name = var.lambda-name
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_function.lambda_handler"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "python3.12"
+}
+
+# Creation of Role for Lambda Function
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "notification-system-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+# Attaching the AmazonSNSFullAccess policy
+resource "aws_iam_role_policy_attachment" "attach-sns-policy" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
+}
+
+# Attaching the AmazonSQSFullAccess policy
+resource "aws_iam_role_policy_attachment" "attach-sqs-policy" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+}
+
+
+# ==== DATA SOURCE SECTION ====
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/lambda_function.py"
+  output_path = "${path.module}/lambda/lambda_function_payload.zip"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}

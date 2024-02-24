d70a260f6033ec7103386f29e28a35e8c791d4f2
Cristobal Grau <cristobalgrau@gmail.com>
2024-02-24 21:44:50 +0000

    #7 Added ENV VARS for Lambda and created WatchLog Group for Lambda

--- main.tf
+++ main.tf
@@ -24,6 +24,43 @@ provider "aws" {
   }
 }
 
+# ==== DATA SOURCE SECTION ====
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/lambda_function.py"
+  output_path = "${path.module}/lambda/lambda_function_payload.zip"
+}
+
+# Policy document for Lambda to Assume Role
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+# Policy document to create Logs for Lambda: AWSLambdaBasicExecutionRole
+data "aws_iam_policy_document" "lambda_logging" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
 # ==== STORAGE SECTION ====
 
 resource "aws_s3_bucket" "project-bucket" {
@@ -64,6 +101,19 @@ resource "aws_lambda_function" "project_lambda" {
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = "python3.12"
+
+  # ENV VARS for Lambda
+  environment {
+    variables = {
+      SNS_TOPIC_ARN = aws_sns_topic.project_sns.arn
+      SQS_QUEUE_URL = aws_sqs_queue.project_sqs.url
+    }
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.project_logs,
+  ]
 }
 
 # Creation of Role for Lambda Function
@@ -85,23 +135,24 @@ resource "aws_iam_role_policy_attachment" "attach-sqs-policy" {
 }
 
 
-# ==== DATA SOURCE SECTION ====
+# ==== CLOUDWATCH LOG SECTION ====
 
-data "archive_file" "lambda" {
-  type        = "zip"
-  source_file = "${path.module}/lambda/lambda_function.py"
-  output_path = "${path.module}/lambda/lambda_function_payload.zip"
+# Creation of Cloudwatch Log Group
+resource "aws_cloudwatch_log_group" "project_logs" {
+  name              = "/aws/lambda/${var.lambda-name}"
+  retention_in_days = 14
 }
 
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
+# Policy for Lambda Logging
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+  policy      = data.aws_iam_policy_document.lambda_logging.json
+}
 
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
+# Attaching Lambda Logging Policy to the Lambda Role
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
 }
